a62f3be813bf8d0ee23b7f717c0c1b59897e89c3
yslee <leeys9423@gmail.com>
2024-08-07 00:10:08 +0900

    add User Domain

---
+++ src/main/java/com/springjwt/entity/User.java
@@ -0,0 +1,22 @@
+package com.springjwt.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    private String username;
+    private String password;
+    private String role;
+}
